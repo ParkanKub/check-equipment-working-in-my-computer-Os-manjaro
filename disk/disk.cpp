5eef94119c65432896d2f8e400fd5c9529e4ba4b
ParkanKub <142525625+ParkanKub@users.noreply.github.com>
2026-09-01 13:43:59 +0700

    disk

--- disk/disk.cpp
+++ disk/disk.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <thread>
+#include <chrono>
+
 
 using namespace std;
 
-int main() {
+long long getDiskRead(){
     ifstream disk("/proc/diskstats");
     string line;
-    long long readSectors = 0;
-    long long writeSectors = 0;
     while(getline(disk,line)){
         istringstream ss(line);
         string name;
-        long long value;
+        int major, minor;
+        long long readIO, readMerges, readSectors , readTime;
+        ss >> major >> minor >> name >> readIO >> readMerges >> readSectors >> readTime;
+        if (name == "nvme0n1"){
+            return readSectors;
+        }
     }
+    return 0;
+}
 
+int main() {
+    long long initialRead = getDiskRead();
+    this_thread::sleep_for(chrono::seconds(5));
+    long long finalRead = getDiskRead();
+    long long readSectors = finalRead - initialRead;
+    long long readBytes = readSectors * 512;
+    double mb = (double)readBytes / (1024 * 1024);
+    cout << "Disk Usage: " << mb << " MB/s\n";
 }
