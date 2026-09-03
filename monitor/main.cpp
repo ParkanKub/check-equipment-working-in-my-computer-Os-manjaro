19792b42e20f1b272f4de7cf0200b15a2f4670f9
ParkanKub <142525625+ParkanKub@users.noreply.github.com>
2026-09-03 23:55:46 +0700

    start qt

--- monitor/main.cpp
+++ monitor/main.cpp
@@ -1,4 +1,7 @@
 #include "cpu/cpu.h"
+#include <QApplication>
+#include <QWidget>
+
 #include <iostream>
 using namespace std;
 
@@ -7,9 +10,14 @@ long long getNumberOfCPUs()
     return getCPUName().size();
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     long long numberOfCPUs = getNumberOfCPUs();
 
     cout << "Number of CPUs: " << numberOfCPUs << endl;
+    QApplication a(argc, argv);
+    QWidget window;
+    window.resize(500, 300);
+    window.show();
+    return a.exec();
 }
