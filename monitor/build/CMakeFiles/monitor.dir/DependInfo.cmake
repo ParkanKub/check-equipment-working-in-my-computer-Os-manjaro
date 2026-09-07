88842ff71b6bb399791f6853ae9e3fea104b60bb
ParkanKub <142525625+ParkanKub@users.noreply.github.com>
2026-09-07 12:25:37 +0700

    test build app

---
+++ monitor/build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kob_yai/Documents/GitHub/check-equipment-working-in-my-computer-Os-manjaro/monitor/cpu/cpu.cpp" "CMakeFiles/monitor.dir/cpu/cpu.cpp.o" "gcc" "CMakeFiles/monitor.dir/cpu/cpu.cpp.o.d"
+  "/home/kob_yai/Documents/GitHub/check-equipment-working-in-my-computer-Os-manjaro/monitor/disk/disk.cpp" "CMakeFiles/monitor.dir/disk/disk.cpp.o" "gcc" "CMakeFiles/monitor.dir/disk/disk.cpp.o.d"
+  "/home/kob_yai/Documents/GitHub/check-equipment-working-in-my-computer-Os-manjaro/monitor/main.cpp" "CMakeFiles/monitor.dir/main.cpp.o" "gcc" "CMakeFiles/monitor.dir/main.cpp.o.d"
+  "/home/kob_yai/Documents/GitHub/check-equipment-working-in-my-computer-Os-manjaro/monitor/network/network.cpp" "CMakeFiles/monitor.dir/network/network.cpp.o" "gcc" "CMakeFiles/monitor.dir/network/network.cpp.o.d"
+  "/home/kob_yai/Documents/GitHub/check-equipment-working-in-my-computer-Os-manjaro/monitor/ram/ram.cpp" "CMakeFiles/monitor.dir/ram/ram.cpp.o" "gcc" "CMakeFiles/monitor.dir/ram/ram.cpp.o.d"
+  "" "monitor" "gcc" "CMakeFiles/monitor.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
